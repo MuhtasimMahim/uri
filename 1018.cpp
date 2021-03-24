f74a0603a7967023c56cbf9fe533e2a17a95676d
MuhtasimMahim <48066510+MuhtasimMahim@users.noreply.github.com>
2021-03-24 22:08:27 +0600

    1018.cpp
    Just used modulus. Simple :)

--- 1018.cpp
+++ 1018.cpp
@@ -1,50 +1,23 @@
 #include <iostream>
-
+#include<iomanip>
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(false);
-    int v, cem = 0, cinq = 0, vin = 0, dez = 0, cinc = 0, dois = 0;
-    cin >> v;
-    int div = v / 100, um = v;
-    if (div >= 1) {
-        cem = div;
-        um -= 100 * cem;
-    }
-    div = um / 50;
-    if (div >= 1) {
-        cinq = div;
-        um -= 50 * cinq;
-    }
-    div = um / 20;
-    if (div >= 1) {
-        vin = div;
-        um -= 20 * vin;
-    }
-    div = um / 10;
-    if (div >= 1) {
-        dez = div;
-        um -= 10 * dez;
-    }
-    div = um / 5;
-    if (div >= 1) {
-        cinc = div;
-        um -= 5 * cinc;
-    }
-    div = um / 2;
-    if (div >= 1) {
-        dois = div;
-        um -= 2 * dois;
-    }
-    cout << v << "\n";
-    cout << cem << " nota(s) de R$ 100,00\n";
-    cout << cinq << " nota(s) de R$ 50,00\n";
-    cout << vin << " nota(s) de R$ 20,00\n";
-    cout << dez << " nota(s) de R$ 10,00\n";
-    cout << cinc << " nota(s) de R$ 5,00\n";
-    cout << dois << " nota(s) de R$ 2,00\n";
-    cout << um << " nota(s) de R$ 1,00\n";
-
-    return 0;
+int main() {
+int x;
+cin >> x;
+cout << x << "\n";
+cout << x/100 << " nota(s) de R$ 100,00" "\n";
+x = x%100;
+cout << x/50 << " nota(s) de R$ 50,00" "\n";
+x= x%50;
+cout << x/20 << " nota(s) de R$ 20,00" "\n";
+x= x%20;
+cout << x/10 << " nota(s) de R$ 10,00" "\n";
+x= x%10;
+cout << x/5 << " nota(s) de R$ 5,00" "\n";
+x= x%5;
+cout << x/2 << " nota(s) de R$ 2,00" "\n";
+x= x%2;
+cout << x/1 << " nota(s) de R$ 1,00" "\n";
+return 0;
 }
